298cd9fda58ef903e32a7d46cf45cacb393a8772
yujong <yujong4170@gmail.com>
2023-01-26 16:20:14 +0900

    yujong

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -7,12 +7,42 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 @Repository
 public class MemberRepository {
 
     @PersistenceContext
     private EntityManager em;
+
+
+    //< '회원 저장'하기 >
+    public void save(Member member){
+
+        em.persist(member);
+    }
+
+
+    //< '개별 회원(1명) 조회'하기 >
+    //'매개변수로 들어온 id에 해당하는 회원'을 DB에서 찾아와서 '그 회원을 리턴'해줌
+    public Member findOne(Long id){
+
+        Member member = em.find(Member.class, id); //'반환타입', '들어온 id값' 순
+        return member;
+
+    }
+
+
+    //< '전체 회원(N명) 조회'하기 >
+    public List<Member> findAll() {
+
+        //'JPQL 작성해서 이를 통해 전체 회원 목록 가져오기>
+        List<Member> result = em.createQuery("select m from Member", Member.class)
+                .getResultList();
+
+        return result;
+
+    }
 
 
 }
--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -15,41 +15,41 @@ import org.springframework.transaction.annotation.Transactional;
 @SpringBootTest //스프링부트로 테스트해야 하기 때문에 붙여줌
 public class MemberRepositoryTest { //'레펏 MemberRepository'가 잘 작동하는지 테스트하는 것
 
-    @Autowired
-    MemberRepository memberRepository; //'레펏 MemberRepository'를 테스트하려면, 여기 테스트 클래스로 그것을
-                                       //가지고 들어와야 하니깐, '@Autowired' 붙임.
-
-
-
-    @Transactional //- 'EntityManager를 통한 모든 데이터 변경'은 '트랜잭션 안'에서 이루어져야 한다!
-                   //- '@Transactional'은 '테스트에 들어가 있으면', '테스트 완료 후 다 롤백해버림'.
-    @Rollback(false) //- 그렇게 이 어노테이션을 쓰면, 롤백 안시킨다! 그럼 이제 롤백 안하고 바로 커밋해버린다
-    @Test
-    public void testMember() throws Exception{
-
-
-        //전제 given
-        Member member = new Member();
-        member.setUsername("member");
-
-
-        //조건 when
-        Long savedId = memberRepository.save(member); //DB에 넣음
-        Member findMember = memberRepository.find(savedId);//DB에 'Member 객체'가 잘 저장됐는는를 테스트
-
-
-        //검증(then)
-        //[ 'JPA와 DB 설정, 동작확인'강 11:00~ ]
-        //- 'findMember.getId()': 위에서 'DB에 성공적으로 저장됐다고 가정한 '회원 id''와
-        //- 'member.getId()': '엔티티 Member 객체'에 있는 '회원 id'가 
-        //- 'isEqualTo()': 동일한지 여부를
-        //- 'Assertions.assertThat()': 검증한다
-        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId()); 
-        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
-        Assertions.assertThat(findMember).isEqualTo(member); //- 'DB에 저장된 회원 id'와 '엔티티 Member 객체
-                                                             //에 있는 회원 id'가 동일한지 여부
-                                                             //- 결과: true
-
+//    @Autowired
+//    MemberRepository memberRepository; //'레펏 MemberRepository'를 테스트하려면, 여기 테스트 클래스로 그것을
+//                                       //가지고 들어와야 하니깐, '@Autowired' 붙임.
+//
+//
+//
+//    @Transactional //- 'EntityManager를 통한 모든 데이터 변경'은 '트랜잭션 안'에서 이루어져야 한다!
+//                   //- '@Transactional'은 '테스트에 들어가 있으면', '테스트 완료 후 다 롤백해버림'.
+//    @Rollback(false) //- 그렇게 이 어노테이션을 쓰면, 롤백 안시킨다! 그럼 이제 롤백 안하고 바로 커밋해버린다
+//    @Test
+//    public void testMember() throws Exception{
+//
+//
+//        //전제 given
+//        Member member = new Member();
+//        member.setUsername("member");
+//
+//
+//        //조건 when
+//        Long savedId = memberRepository.save(member); //DB에 넣음
+//        Member findMember = memberRepository.find(savedId);//DB에 'Member 객체'가 잘 저장됐는는를 테스트
+//
+//
+//        //검증(then)
+//        //[ 'JPA와 DB 설정, 동작확인'강 11:00~ ]
+//        //- 'findMember.getId()': 위에서 'DB에 성공적으로 저장됐다고 가정한 '회원 id''와
+//        //- 'member.getId()': '엔티티 Member 객체'에 있는 '회원 id'가
+//        //- 'isEqualTo()': 동일한지 여부를
+//        //- 'Assertions.assertThat()': 검증한다
+//        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+//        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+//        Assertions.assertThat(findMember).isEqualTo(member); //- 'DB에 저장된 회원 id'와 '엔티티 Member 객체
+//                                                             //에 있는 회원 id'가 동일한지 여부
+//                                                             //- 결과: true
+//
 
 
     }
